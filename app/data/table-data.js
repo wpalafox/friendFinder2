1bfe476453498af3329093ee9010d4f2e141a356
Will Palafox <wpalafox@gmail.com>
2016-12-09 14:22:14 -0600

    Changed object structure and used jQuery to capture user values

--- app/data/table-data.js
+++ app/data/table-data.js
@@ -10,18 +10,10 @@ var tableArray = [
    { 
  	friendName: "Carlton",
  	photoLink: "http://s3.amazonaws.com/rapgenius/carltonbanks.jpg",
-   question1: 	"4",
-   question2: 	"1",
-   question3: 	"2",
-   question4: 	"4",
-   question5: 	"2",
-   question6: 	"3",
-   question7: 	"3",
-   question8: 	"1",
-   question9: 	"2",
-   question10: "4" 				   
+   selections: ["4", "3", "1", "2", "4", "2", "2", "5", "4", "2"]
+   
+   }, 
 
-					}, 
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
